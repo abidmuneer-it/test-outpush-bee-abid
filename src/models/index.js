2ca3efc9c7e3b2d8b00e1f18e3ec1e113305ef18
abid muneer <abidmuneer93@gmail.com>
2023-05-19 18:05:14 +0500

    Task completed

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.Token = require('./token.model');
 module.exports.User = require('./user.model');
 module.exports.Website = require('./website.model');
+module.exports.Publisher = require('./publisher.model');
